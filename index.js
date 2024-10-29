18c86d2d5a8bf4dcac22d04c0ae208a024edad97
Ayushbahugunajii <ayushbahuguna76@gmail.com>
2024-10-29 19:30:35 +0530

    fixed

--- index.js
+++
@@ -1,9 +0,0 @@
-var age=25;
-var name='ayush';
-var dob='07/08/2004';
-function ayush (){
-console.log(age);  
-}
-function demo() {
-    console.log('ayush');
-}
--- scritpt.js
+++
@@ -1,8 +0,0 @@
-var a="ayush";
-const b=12;
-function ayush() {
-    console.log(
-        "hey"
-    );
-    
-}
